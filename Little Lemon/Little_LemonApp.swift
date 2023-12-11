1d7c7ab4525d1071f310c917ba295766a3231c92
Antonio Lopes <aclopesjr@email.com>
2023-12-11 15:17:13 -0300

    Initial Commit

---
+++ Little Lemon/Little_LemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  Little_LemonApp.swift
+//  Little Lemon
+//
+//  Created by Antonio Lopes on 11/12/23.
+//
+
+import SwiftUI
+
+@main
+struct Little_LemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
